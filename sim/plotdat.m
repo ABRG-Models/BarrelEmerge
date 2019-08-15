b3b21933dfe7ef1185f2ba94a37f77a2c39803d8
Seb James <seb.james@sheffield.ac.uk>
2019-08-15 17:11:35 +0100

    Minor changes

--- sim/plotdat.m
+++ sim/plotdat.m
@@ -1,8 +1,10 @@
 a = csvread ('data.csv');
 
+fnum = 1;
 for doms=unique(a(:,1))'
-    figure()
-    scatter(a(:,2),a(:,3))
+    figure(fnum);
+    clf;
+    scatter(a(:,2),a(:,3),"filled")
     hold on;
     b = a(a(:,1)==doms, :);
     for idx=1:length(b(:,1))
@@ -12,4 +14,5 @@ for doms=unique(a(:,1))'
             plot ( [b(idx,2),b(idx,4)], [b(idx,3),b(idx,5)], 'color',[b(idx,1),doms./length(a(:,1)),1-b(idx,1)]);
         end
     end
+    fnum = fnum + 1;
 end
