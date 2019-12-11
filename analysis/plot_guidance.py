d71880b1d3cf4f482e0bea5a0694cd3506bfaf46
Seb James <seb.james@sheffield.ac.uk>
2019-12-11 09:58:58 +0000

    Brings plot_guidance up to date

--- analysis/plot_guidance.py
+++ analysis/plot_guidance.py
@@ -1,25 +1,18 @@
 # To access argv and also include the include dir
+import os
 import sys
 sys.path.insert (0, './include')
 import numpy as np
-# Import data loading code
-import load as ld
-# Import MY plotting code:
+import BarrelData as bd
 import plot as pt
-import matplotlib
 import matplotlib.pyplot as plt
-# Direct HDF5 access
-import h5py
-# My domcentres linear fit code:
-import domcentres as dc
-import os
 
 # Get target x/y hex to show trace for and the time step to show the
 # map for from the arguments:
 if len(sys.argv) < 2:
     print('Provide logdirname on cmd line please. Optionally provide molecule index.')
     exit(1)
-logdirname = sys.argv[1]
+logdirname = sys.argv[1].rstrip ('/')
 
 # molecule index
 if len(sys.argv) > 2:
@@ -27,12 +20,13 @@ if len(sys.argv) > 2:
 else:
     mi = 0
 
-# Read data
-(x, y, gmatrix) = ld.readGuidance (logdirname)
-
-# Plot guidance...
-shp = np.shape(gmatrix)
-print ('gmatrix shape: {0}'.format(shp))
+# Read the data
+bdo = bd.BarrelData()
+bdo.loadSimData = False
+bdo.loadAnalysisData = False
+bdo.loadGuidance = True
+bdo.loadPositions = True
+bdo.load (logdirname)
 
 print ('basename: {0}'.format(os.path.basename(logdirname)))
 winwidth = 12
@@ -68,10 +62,10 @@ if mi==0:
 else:
     pl.cmap = plt.cm.Reds
 
-f1 = pl.surface (gmatrix[:,mi], x, y)
+print ("bdo.g shape: {0}".format (np.shape(bdo.g)))
+f1 = pl.surface (bdo.g[:,mi], bdo.x, bdo.y)
 
-# Put saving into the class?
-mapname = '{0}_guide{1}.png'.format(os.path.basename(logdirname), mi)
+mapname = 'plots/{0}_guide{1}.png'.format(os.path.basename(logdirname), mi)
 plt.savefig (mapname, dpi=300, transparent=True)
 
 plt.show()
