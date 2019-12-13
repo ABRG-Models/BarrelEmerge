869341bae4d6975e2b58372976e9eb965581eeac
Seb James <seb.james@sheffield.ac.uk>
2019-12-13 17:27:00 +0000

    Corrected scripts for movie making

--- analysis/plots/cid_all/movie.sh
+++ analysis/plots/cid_all/movie.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-touch 41N2M_thalguide_eps150_c_id.mp4
-ffmpeg -r 4 -i 41N2M_thalguide_eps150_c_id_%06d.png -vb 5MB -vcodec mpeg4 41N2M_thalguide_eps150_c_id.mp4 -y
+touch 41N2M_thalguide_eps150_movie_c_id.mp4
+ffmpeg -r 10 -i 41N2M_thalguide_eps150_movie_c_id_%06d.png -vb 5MB -vcodec mpeg4 41N2M_thalguide_eps150_movie_c_id.mp4 -y
