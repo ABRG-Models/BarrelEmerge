fbf323b394ae7642b1c239676be526ec626430f1
Seb James <seb.james@sheffield.ac.uk>
2020-04-27 13:02:53 +0100

    Move plotting code into external function

--- analysis/plot_paramsearch.py
+++ analysis/plot_paramsearch.py
@@ -5,158 +5,41 @@
 
 # To access argv and also include the include dir
 import sys
-import csv
 sys.path.insert (0, './include')
 import numpy as np
-# Import MY plotting code:
-import plot as pt
 import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import AxesGrid
-import sebcolour
-col = sebcolour.Colour
+import paramplot
 
 # Set plotting defaults
+#matplotlib.use('TkAgg') # cleaner likely to work
+#matplotlib.use('Qt5Agg') # Ugly, sometimes default
+matplotlib.use('TkCairo') # Install pycairo to be able to use this
 fs = 12
 fnt = {'family' : 'DejaVu Sans',
        'weight' : 'regular',
        'size'   : fs}
 matplotlib.rc('font', **fnt)
 
-show_xy = 0
+#
+# Load Data
+#
 
-F1 = plt.figure (figsize=(20,12))
-if show_xy:
-    F2 = plt.figure (figsize=(8,4))
 # Cols in sdata are: k,D,alphabeta,alpha,beta,epsilon,t,hondadelta,sos_dist,area_diff
 sdata = np.genfromtxt ('postproc/paramsearch_k3_dncomp.csv', delimiter=",", names=True)
 # sdata is a numpy 'structured array' with named fields.
 
-# Get max/min for data ranges
-sos_max = max(sdata[:]['sos_dist'])
-sos_min = min(sdata[:]['sos_dist'])
-print ('sos max/min: {0}/{1}'.format (sos_max, sos_min))
-area_max = max(sdata[:]['area_diff'])
-area_min = min(sdata[:]['area_diff'])
-honda_max = max(sdata[:]['hondadelta'])
-honda_min = min(sdata[:]['hondadelta'])
-# Extent of the data range for plotting
-plot_extent = [-0.5, 5.5, -0.5, 5.5]
+#
+# Plot Data
+#
 
-ttarg = 25000
-# Plot heatmaps where we extract rows where epsilon = 100, t = 25000
-# Can cycle through epsilon and t.
-i = 1
-
-# Original layout
-column_tag = 'epsilon'
-x_tag = 'D'
-y_tag = 'alphabeta'
-
-# D is column:
-#column_tag = 'D'
-#x_tag = 'epsilon'
-#y_tag = 'alphabeta'
-
-# alphabeta is column:
-#column_tag = 'alphabeta'
-#x_tag = 'D'
-#y_tag = 'epsilon'
-
-colall = np.sort(np.unique(sdata[:][column_tag]))
-x_all = np.sort(np.unique(sdata[:][x_tag]))
-y_all = np.sort(np.unique(sdata[:][y_tag]))
-print ('x_all: {0}'.format(x_all))
-
-x_tick_list = list(range(0,len(x_all)))
-y_tick_list = list(range(0,len(y_all)))
-# Not sure why I have to subtract 0.5 from each here...
-y_tick_list = [yt-0.5 for yt in y_tick_list]
-
-for coltarg in colall:
-
-    mapdat = sdata[np.logical_and(sdata[:][column_tag] == coltarg,
-                                  sdata[:]['t'] == ttarg)]
-
-    # Now sort mapdat on cols of interest
-    mapdat = np.sort (mapdat, order=(y_tag, x_tag))
-    # Need to reshape hond from 36x1 into 6x6
-    hond6x6 = mapdat[:]['hondadelta'].reshape((len(y_all), len(x_all)))
-    sos6x6 = mapdat[:]['sos_dist'].reshape((len(y_all), len(x_all)))
-    area6x6 = mapdat[:]['area_diff'].reshape((len(y_all), len(x_all)))
-    # Can heat map these to prove the ordering is sensible:
-    x6x6 =  mapdat[:][x_tag].reshape((len(y_all), len(x_all)))
-    y6x6 =  mapdat[:][y_tag].reshape((len(y_all), len(x_all)))
-
-    # In separate figure, show the x and y graphs
-    if show_xy:
-        imax1 = F2.add_subplot(1,2,1)
-        axim1 = imax1.imshow (x6x6, cmap='hot',
-                              extent=plot_extent, interpolation='none')
-        imax1.set_xlabel(x_tag)
-        imax2 = F2.add_subplot(1,2,2)
-        axim2 = imax2.imshow (y6x6, cmap='hot',
-                              extent=plot_extent, interpolation='none')
-        imax2.set_ylabel(y_tag)
-        #plt.show() ## debug
-
-    print ('adding subplot; i={0} len(colall)={1}'.format(i,len(colall)))
-    ax = F1.add_subplot(3,len(colall),i)
-
-    im = ax.imshow (hond6x6, cmap='magma_r', vmin=honda_min, vmax=honda_max,
-                    extent=plot_extent, interpolation='none')
-    tlist = []
-    for j in range(0,len(x_all)): tlist.append('{0:.2f}'.format(x6x6[0,j]))
-    plt.xticks (x_tick_list, tlist)
-    tlist = ['']
-    for j in range(0,len(y_all)): tlist.append('{0:.1f}'.format(y6x6[j,0]))
-    plt.yticks (y_tick_list, tlist)
-    if coltarg == colall[0]:
-        ax.set_ylabel('{0} : honda'.format(y_tag))
-    ax.set_title('{0}={1:.2f}'.format(column_tag,coltarg))
-    ax.set_xlabel(x_tag)
-
-    ax1 = F1.add_subplot(3,len(colall),len(colall)+i)
-    im1 = ax1.imshow (sos6x6, cmap='plasma_r', vmin=sos_min, vmax=sos_max,
-                    extent=plot_extent, interpolation='nearest')
-    tlist = []
-    for j in range(0,len(x_all)): tlist.append('{0:.2f}'.format(x6x6[0,j]))
-    plt.xticks (x_tick_list, tlist)
-    tlist = ['']
-    for j in range(0,len(y_all)): tlist.append('{0:.1f}'.format(y6x6[j,0]))
-    plt.yticks (y_tick_list, tlist)
-    if coltarg == colall[0]:
-        ax1.set_ylabel('{0} : sos'.format(y_tag))
-    ax1.set_xlabel(x_tag)
-
-    ax2 = F1.add_subplot(3,len(colall),(2*len(colall))+i)
-    im2 = ax2.imshow (area6x6, cmap='viridis_r', vmin=area_min, vmax=area_max,
-                    extent=plot_extent, interpolation='nearest')
-    tlist = []
-    for j in range(0,len(x_all)): tlist.append('{0:.2f}'.format(x6x6[0,j]))
-    plt.xticks (x_tick_list, tlist)
-    tlist = ['']
-    for j in range(0,len(y_all)): tlist.append('{0:.1f}'.format(y6x6[j,0]))
-    plt.yticks (y_tick_list, tlist)
-    if coltarg == colall[0]:
-        ax2.set_ylabel('{0} : areadiff'.format(y_tag))
-    ax2.set_xlabel(x_tag)
-
-    i += 1
-
-# Deal with colorbars here. Manual fiddling to get position correct
-cb_height = 0.151
-cb_wid = 0.01
-cb_xpos = 0.92
-cb_ax = F1.add_axes([cb_xpos, 0.692, cb_wid, cb_height])
-cbar = F1.colorbar (im, cax=cb_ax)
-
-cb_ax1 = F1.add_axes([cb_xpos, 0.42, cb_wid, cb_height])
-cbar1 = F1.colorbar (im1, cax=cb_ax1)
-
-cb_ax2 = F1.add_axes([cb_xpos, 0.148, cb_wid, cb_height])
-cbar2 = F1.colorbar (im2, cax=cb_ax2)
-
+F1 = plt.figure (figsize=(20,12))
+##F2 = plt.figure (figsize=(20,12))
+##F3 = plt.figure (figsize=(20,12))
+#                               col          x            y
+paramplot.paramplot (sdata, F1, 'epsilon',   'D',         'alphabeta');
+##paramplot.paramplot (sdata, F2, 'D',         'alphabeta', 'epsilon');
+##paramplot.paramplot (sdata, F3, 'alphabeta', 'epsilon',   'D');
 
 plt.show()
 exit (0)
