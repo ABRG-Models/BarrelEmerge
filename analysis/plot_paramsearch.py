9b7a750578def3b4ad3877debe248ac6c78cdf9a
Seb James <seb.james@sheffield.ac.uk>
2020-04-27 10:51:05 +0100

    Remove some commented code

--- analysis/plot_paramsearch.py
+++ analysis/plot_paramsearch.py
@@ -23,7 +23,7 @@ fnt = {'family' : 'DejaVu Sans',
        'size'   : fs}
 matplotlib.rc('font', **fnt)
 
-F1 = plt.figure (figsize=(18,12))
+F1 = plt.figure (figsize=(20,12))
 
 # Cols in sdata are: k,D,alphabeta,alpha,beta,epsilon,t,hondadelta,sos_dist,area_diff
 sdata = np.genfromtxt ('postproc/paramsearch_k3_dncomp.csv', delimiter=",", names=True)
@@ -41,7 +41,7 @@ honda_min = min(sdata[:]['hondadelta'])
 # Extent of the data range for plotting
 plot_extent = [-0.5, 5.5, -0.5, 5.5]
 
-ttarg = 15000
+ttarg = 25000
 # Plot heatmaps where we extract rows where epsilon = 100, t = 25000
 # Can cycle through epsilon and t.
 i = 1
@@ -53,13 +53,6 @@ for epstarg in [50, 100, 150, 200, 300]:
     # Now sort mapdat on cols of interest
     mapdat = np.sort (mapdat, order=('alphabeta','D'))
 
-    # Take log of Dcol and abcol as these vary exponentially
-    #logD = np.log (mapdat[:]['D'])
-    #logab = np.log (mapdat[:]['alphabeta'])
-    #mapdat[:]['D'] = logD
-    #mapdat[:]['alphabeta'] = logab
-    #print (mapdat)
-
     # Need to reshape hond from 36x1 into 6x6
     hond6x6 = mapdat[:]['hondadelta'].reshape((6, 6))
     sos6x6 = mapdat[:]['sos_dist'].reshape((6, 6))
@@ -67,15 +60,9 @@ for epstarg in [50, 100, 150, 200, 300]:
     # Can heat map these to prove the ordering is sensible:
     D6x6 =  mapdat[:]['D'].reshape((6, 6))
     ab6x6 =  mapdat[:]['alphabeta'].reshape((6, 6))
-    #print (ab6x6)
 
-    #print (hond6x6)
-    # This shows hondadelta ordered by alphabeta on y and D on x
     ax = F1.add_subplot(3,5,i)
-    #nonnan = ~np.isnan(hond6x6)
-    #h = hond6x6[nonnan]
-    #print (h)
-    # NB: An alternative to imshow() is pcolor()
+
     im = ax.imshow (hond6x6, cmap='hot', vmin=honda_min, vmax=honda_max,
                     extent=plot_extent, interpolation='none')
     tlist = []
@@ -83,14 +70,11 @@ for epstarg in [50, 100, 150, 200, 300]:
     plt.xticks ([0,1,2,3,4,5], tlist)
     tlist = ['']
     for j in range(0,5): tlist.append('{0:.1f}'.format(ab6x6[j,0]))
-    # Special for 100:
-    tlist.append('{0:d}'.format(int(ab6x6[5,0])))
+    tlist.append('{0:d}'.format(int(ab6x6[5,0]))) # Special format for 100:
     plt.yticks ([-0.5,0,1,2,3,4,5], tlist)
     if epstarg == 50:
         ax.set_ylabel('alphabeta : honda')
     ax.set_title('$\epsilon$={0}'.format(epstarg))
-#    if epstarg == 300:
-#        F1.colorbar(im, ax=ax)
     ax.set_xlabel('D')
 
     ax1 = F1.add_subplot(3,5,5+i)
@@ -101,13 +85,10 @@ for epstarg in [50, 100, 150, 200, 300]:
     plt.xticks ([0,1,2,3,4,5], tlist)
     tlist = ['']
     for j in range(0,5): tlist.append('{0:.1f}'.format(ab6x6[j,0]))
-    # Special for 100:
     tlist.append('{0:d}'.format(int(ab6x6[5,0])))
     plt.yticks ([-0.5,0,1,2,3,4,5], tlist)
     if epstarg == 50:
         ax1.set_ylabel('alphabeta : sos')
-#    if epstarg == 300:
-#        F1.colorbar(im1, ax=ax1)
     ax1.set_xlabel('D')
 
     ax2 = F1.add_subplot(3,5,10+i)
@@ -118,13 +99,10 @@ for epstarg in [50, 100, 150, 200, 300]:
     plt.xticks ([0,1,2,3,4,5], tlist)
     tlist = ['']
     for j in range(0,5): tlist.append('{0:.1f}'.format(ab6x6[j,0]))
-    # Special for 100:
     tlist.append('{0:d}'.format(int(ab6x6[5,0])))
     plt.yticks ([-0.5,0,1,2,3,4,5], tlist)
     if epstarg == 50:
         ax2.set_ylabel('alphabeta : areadiff')
-#    if epstarg == 300:
-#        F1.colorbar(im2, ax=ax2)
     ax2.set_xlabel('D')
 
     i += 1
