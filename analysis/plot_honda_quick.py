0fedd542cf63b7292f1a430e4f287c69feb1af6e
Seb James <seb.james@sheffield.ac.uk>
2020-05-27 17:24:38 +0100

    Updated metric vs t plotting

--- analysis/plot_honda_quick.py
+++ analysis/plot_honda_quick.py
@@ -28,35 +28,40 @@ F1 = plt.figure (figsize=(9,8))
 
 t1_masked = np.load ('postproc/honda_t.npy')/10000
 hondadelta = np.load ('postproc/honda_delta.npy')
-area_measure =  np.load ('postproc/area_measure.npy')
+area_diff =  np.load ('postproc/area_diff.npy')
+locn_vs_t =  np.load ('postproc/locn_vs_t.npy')
 
 xmax = max(t1_masked)
 xmax = xmax[0]
 print ('xmax = {0}'.format(xmax))
-ax1 = F1.add_subplot(1,1,1)
+
+ax1 = F1.add_subplot(2,1,1)
+
 l1, = ax1.plot(t1_masked, hondadelta, 'o', markersize=12, color=col.red, label='Honda $\delta$')
 
-l2, = ax1.plot((0,xmax), (0.022, 0.022), '--', color=col.red, linewidth=3, label="Fig 1A")
-#l2, = ax1.plot((0,xmax), (0.055, 0.055), '--', color=col.red, linewidth=3, label="good (S&W)")
-#l3, = ax1.plot((0,xmax), (0.15, 0.15), '-.', color=col.black, linewidth=3, label="awful (non Dirichlet)")
+# 0.054 is Senft and Woolsey's result for barrels (mouse 0,054, other rodents about 0.055)
+l2, = ax1.plot((0,xmax), (0.055, 0.055), '--', color=col.red, linewidth=3, label="good (S&W)")
 
-ax2 = ax1.twinx()
+ax2 = F1.add_subplot(2,1,2)
 
-l4, = ax2.plot(t1_masked, area_measure, 's', markersize=12, color=col.black)
+l3, = ax2.plot(t1_masked, area_diff, 'v', markersize=12, color=col.black)
 
-ax1.set_xlabel ('10k steps')
-ax1.set_ylabel ('$\delta$',rotation=0)
-ax2.set_ylabel ('$\eta$',rotation=0)
+ax3 = ax2.twinx()
+l4, = ax3.plot(t1_masked, locn_vs_t, 'h', markersize=12, color=col.gray60)
+
+ax2.set_xlabel ('time (10k steps)')
+ax1.set_ylabel ('$\delta$', rotation=0, labelpad=10)
+ax2.set_ylabel ('$\eta$', rotation=0)
+ax3.set_ylabel ('$\omega$', rotation=0)
 ax2.tick_params (axis='y', labelcolor=col.black)
 ax1.set_xlim ((0,xmax))
 ax1.set_ylim ((0,0.3))
-ax2.set_ylim ((0,500))
-ax1.set_xticks ((0,1,2))
-ax1.set_yticks ((0,0.1,0.2,0.3))
-ax2.set_yticks ((0,200,400))
+#ax2.set_ylim ((0,500))
+#ax1.set_xticks ((0,1,2,3,4,5))
+#plt.legend()
 
 plt.tight_layout()
-suffix= '_quick'
-plt.savefig('plots/hondadelta{0}.svg'.format(suffix), transparent=True)
+
+plt.savefig('plots/hondadelta.svg', transparent=True)
 
 plt.show()
