f9715fa11ca11baaae1a2b8ce90b366155236122
Seb James <seb.james@sheffield.ac.uk>
2020-06-10 14:27:44 +0100

    The gamma noise experiment plotting scripts

---
+++ analysis/plot_gamma_noise.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.insert (0, './include')
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import lines
+import sebcolour as sc
+col = sc.Colour()
+
+# Set plotting defaults
+matplotlib.use('TkAgg') # cleaner likely to work
+fnt = {'family' : 'DejaVu Sans', 'weight' : 'regular', 'size' : 18 }
+matplotlib.rc('font', **fnt)
+# IMPORTANT for svg output of text as things that can be edited in inkscape
+plt.rcParams['svg.fonttype'] = 'none'
+
+sdata = np.genfromtxt ('postproc/gamma_noise.csv', delimiter=",", names=True)
+s = np.sort (sdata, order='noise_gain')
+# If required
+#xlbl = (['.03', '.06', '.15', '.6', '1.5'])
+#ylbl = (['.015', '.022', '.03', '.06', '.1'])
+
+noise_gain = s['noise_gain']
+honda = s['hondadelta']
+sos = s['sos_dist']
+area = s['area_diff']
+locn = s['localization']
+
+# These come from the output of plot_paramsearch_paper.py:
+honda_min=0.016603905707597733
+honda_max=0.40066197514533997
+area_min=0.2837541796379569
+area_max=1.0
+locn_min=0.041788052869136255
+locn_max=0.7319451998375573
+sos_min=2.9542248615230577
+sos_max=3.350204398214124
+
+# And plot this longhand here:
+F1 = plt.figure (figsize=(5,12))
+
+ax1 = F1.add_subplot(3,1,1)
+l1, = ax1.plot(noise_gain, honda, 'o-', markersize=12, color=col.red, label='Honda $\delta$')
+
+ax2 = F1.add_subplot(3,1,2)
+l2, = ax2.plot(noise_gain, locn, 'h-', markersize=12, color=col.gray60, label='Locn $\omega$')
+ax3 = F1.add_subplot(3,1,3)
+l3, = ax3.plot(noise_gain, area, 's-', markersize=12, color=col.black, label='Area $\eta$')
+##ax4 = ax3.twinx()
+##l4, = ax4.plot(noise_gain, sos, 'v-', markersize=12, color=col.blue, label='SOS')
+
+# Axis tweaking
+ax1.set_xlabel ('Noise mag.')
+ax1.set_ylabel ('$\delta$', rotation=0, labelpad=30)
+ax2.set_xlabel ('Noise mag.')
+ax2.set_ylabel ('$\omega$', rotation=0, labelpad=30)
+ax3.set_xlabel ('Noise mag.')
+ax3.set_ylabel ('$\eta$', rotation=0, labelpad=30)
+##ax4.set_ylabel ('SOS', rotation=0, labelpad=30)
+#ax2.tick_params (axis='y', labelcolor=col.black)
+#ax1.set_xlim((0,1.0))
+ax1.set_ylim ((honda_min,honda_max))
+ax2.set_ylim ((locn_min,locn_max)) # locn, omega
+#ax2.set_xlim((0,1.0))
+ax3.set_ylim ((area_min,area_max)) # area, eta
+#ax3.set_ylim ((0.52,0.62))
+#ax4.set_ylim ((sos_min,3.6))
+
+lw = 2
+for axis in ['top','bottom','left','right']:
+  ax1.spines[axis].set_linewidth(lw)
+  ax1.tick_params(width=lw)
+  ax2.spines[axis].set_linewidth(lw)
+  ax2.tick_params(width=lw)
+  ax3.spines[axis].set_linewidth(lw)
+  ax3.tick_params(width=lw)
+  ##ax4.spines[axis].set_linewidth(lw)
+  ##ax4.tick_params(width=lw)
+
+plt.tight_layout()
+
+plt.savefig('plots/gamma_noise.svg', transparent=True)
+
+plt.show()
---
+++ analysis/process_gamma_noise.py
@@ -0,0 +1,65 @@
+#
+# Process sensitivity analysis results gained from varying the noise applied to
+# the gamma interaction parameters
+#
+
+import sys
+import os
+sys.path.insert (0, './include')
+import numpy as np
+import BarrelData as bd
+import h5py
+import math
+
+basedir = '/home/seb/gdrive_usfd/data/BarrelEmerge/gamma_noise/'
+# Sensitivity analysis carried out for t=30000
+tpoint = 30000
+
+table = []
+for logdirname in os.listdir(basedir):
+    print ('Log dir: {0}'.format(logdirname))
+
+    spts = logdirname.split('_')
+    ff_gain = 0
+    for pt in spts:
+        if 'gain' in pt:
+            ff_gain = float(pt.rsplit ('gain', 1)[-1])
+    logdirname = basedir + logdirname
+
+    # Load the dirichlet data, domcentres, etc
+    bdo = bd.BarrelData()
+    bdo.loadAnalaysisData = True
+    bdo.loadPositions = False # required for totalarea
+    bdo.loadGuidance = False
+    bdo.loadSimData = True # Need sim data to do any analysis of the c values, e.g. max me - max others.
+    bdo.loadDivisions = False
+    try:
+        print ('Loading {0}'.format(logdirname))
+        bdo.load (logdirname)
+
+        # Clean zeros out of honda delta and sos_distances and make a mask
+        hondadelta = np.ma.masked_equal (bdo.honda, 0)
+        sos_dist =  np.ma.masked_equal (bdo.sos_dist, 0)
+        mask_combined = np.invert(hondadelta.mask | sos_dist.mask)
+        t1_masked = bdo.t_steps[mask_combined].T
+        t1 = t1_masked[:,0]
+        mapdiff = bdo.mapdiff[mask_combined].T
+        area_diff = bdo.area_diff[mask_combined].T
+        area_diff = area_diff / bdo.nhex
+        sos_dist = np.sqrt(sos_dist.compressed()/bdo.N)
+        hondadelta = hondadelta.compressed()
+        bdo.computeLocalization()
+
+        tableline = [ff_gain, hondadelta[0], sos_dist[0], area_diff[0,0], bdo.locn_vs_t[0]]
+        table.append (tableline)
+    except:
+        print ('Failed to load BarrelData object')
+        tableline = [ff_gain, math.nan, math.nan, math.nan, math.nan]
+        table.append (tableline)
+
+import csv
+with open('postproc/gamma_noise.csv', 'w', newline='\n') as csvfile:
+    cw = csv.writer (csvfile, delimiter=',')
+    cw.writerow (['noise_gain','hondadelta','sos_dist','area_diff','localization'])
+    for tableline in table:
+        cw.writerow (tableline)
