1c244a9cf2bee26adc0339e8dbebcc0574183f36
Seb James <seb.james@sheffield.ac.uk>
2020-06-11 12:30:38 +0100

    Adds adjacency metrics to the generated csv. This commit contains both the script and the generated csv

--- analysis/process_paramsearch_comp2.py
+++ analysis/process_paramsearch_comp2.py
@@ -117,16 +117,18 @@ for logdirname in os.listdir(basedir):
     bdo = bd.BarrelData()
     bdo.loadAnalaysisData = True
     bdo.loadPositions = False # required for totalarea
-    bdo.loadGuidance = False
+    bdo.loadGuidance = True # adjacency
+    bdo.loadHexFlags = True # adjacency
     bdo.loadSimData = True # Need sim data to do any analysis of the c values, e.g. max me - max others.
     bdo.loadDivisions = False
     try:
         bdo.load (logdirname)
+        bdo.computeAdjacencyMeasure()
     except:
         print ('Failed to load BarrelData object; continue to next')
         # Or maybe add a table line(s) with NANs to aid producing heatmaps and show where the thing failed?
         for tt in [1, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000]:
-            tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, tt, math.nan, math.nan, math.nan, ff_F, math.nan]
+            tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, tt, math.nan, math.nan, math.nan, ff_F, math.nan, math.nan, math.nan]
             table.append (tableline)
         continue
 
@@ -168,6 +170,8 @@ for logdirname in os.listdir(basedir):
     mapdiff = bdo.mapdiff[mask_combined].T
     area_diff = bdo.area_diff[mask_combined].T
     area_diff = area_diff / np.max(area_diff)
+    adj_arrangement = bdo.mean_adjacency_arrangement[mask_combined].T
+    adj_diffmag = bdo.mean_adjacency_differencemag[mask_combined].T
 
     #print ('t shape {0}, t1_masked shape {1}'.format(np.shape(bdo.t),np.shape(t1_masked)))
     # Remove the masked values:
@@ -198,7 +202,7 @@ for logdirname in os.listdir(basedir):
     times = list(bdo.t_steps) #[1, 5000, 10000, 15000, 20000, 25000] # Fill from data?
     for hd in range (0, len(hondadelta)):
         #tableline = [bdo.k, bdo.D, (bdo.meanalpha/20.0), bdo.meanalpha, bdo.meanbeta, bdo.meanepsilon, t1[hd], hondadelta[hd], sos_dist[hd], area_diff[hd,0], bdo.F]
-        tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, t1[hd], hondadelta[hd], sos_dist[hd], area_diff[hd,0], ff_F, bdo.locn_vs_t[hd]]
+        tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, t1[hd], hondadelta[hd], sos_dist[hd], area_diff[hd,0], ff_F, bdo.locn_vs_t[hd], adj_arrangement[hd,0], adj_diffmag[hd,0]]
         # What did this do?
         print ('times: {0}'.format(times))
         print ('t1[hd={0}]: {1}'.format(hd, t1[hd]))
@@ -206,12 +210,12 @@ for logdirname in os.listdir(basedir):
         table.append (tableline)
     for tt in times:
         # Add nans for missing times
-        tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, tt, math.nan, math.nan, math.nan, ff_F, math.nan]
+        tableline = [ff_k, ff_D, ff_ab, ff_a, ff_b, ff_ep, tt, math.nan, math.nan, math.nan, ff_F, math.nan, math.nan, math.nan]
         table.append (tableline)
 
 import csv
 with open(('postproc/paramsearch_k{0}_comp2.csv'.format(k_vals[0])), 'w', newline='\n') as csvfile:
     cw = csv.writer (csvfile, delimiter=',')
-    cw.writerow (['k','D','alphabeta','alpha','beta','epsilon','t','hondadelta','sos_dist','area_diff','F','localization'])
+    cw.writerow (['k','D','alphabeta','alpha','beta','epsilon','t','hondadelta','sos_dist','area_diff','F','localization','adj_arrangement','adj_diffmag'])
     for tableline in table:
         cw.writerow (tableline)
