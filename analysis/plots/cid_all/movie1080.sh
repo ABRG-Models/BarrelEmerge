4d6864cda78f3cc9905e4a870e2ab986a2cdb326
Seb James <seb.james@sheffield.ac.uk>
2020-07-10 20:26:41 +0100

    Updated movie file

--- analysis/plots/cid_all/movie1080.sh
+++ analysis/plots/cid_all/movie1080.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 touch MovieS1.mp4
-ffmpeg -r 10 -i 41N2M_thalguide_eps150_movie_c_id_%06d.png -vb 5MB -s hd1080 -vcodec mpeg4 MovieS1.mp4 -y
+ffmpeg -r 10 -i 41N2M_thalguide_Movie_c_id_%06d.png -vb 5MB -s hd1080 -vcodec mpeg4 MovieS1.mp4 -y
